0ba443b63e95ab2ad6851881b15e176d017261bb
David Lozzi <david.lozzi@slalom.com>
2022-01-14 07:21:57 -0500

    cleanup

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -147,7 +147,7 @@ const Home = () => {
     <>
       <Styled.Wrapper>
         <Styled.Header>
-          <Styled.H1><Link to='/'>Ultimate Star Wars Timeline</Link></Styled.H1>
+          <Styled.H1>Ultimate Star Wars Timeline</Styled.H1>
           <MainMenu />
         </Styled.Header>
         {
